e3f503fab4946fc3c834ccf38013815248c25af8
supremebeing7 <markopolo@gmail.com>
2015-10-05 18:57:17 -0700

    Initial commit

--- app/src/main/java/com/markjlehman/todolist/MainActivity.java
+++ app/src/main/java/com/markjlehman/todolist/MainActivity.java
@@ -5,14 +5,43 @@ import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends ListActivity {
+
+    private ArrayList<String> mTasks = new ArrayList<String>();
+    private Button mTaskButton;
+    private EditText mNewTaskText;
+    private ArrayAdapter<String> mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTaskButton = (Button) findViewById(R.id.addTaskButton);
+        mNewTaskText = (EditText) findViewById(R.id.addTaskField);
+        mTasks = new ArrayList<String>();
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mTasks);
+        setListAdapter(mAdapter);
+        mTaskButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addTask();
+            }
+        });
+
+    }
+
+    private void addTask() {
+        String task = mNewTaskText.getText().toString();
+        mTasks.add(task);
+        mAdapter.notifyDataSetChanged();
     }
 
 
